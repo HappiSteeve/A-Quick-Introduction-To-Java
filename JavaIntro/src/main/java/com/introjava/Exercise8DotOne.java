a45dd61055aa9effff95d4a1d934ea9ee45fc96a
Henry Tirla <Henrytirla@gmail.com>
2021-02-17 04:57:34 +0100

    Exercise 8.1

---
+++ JavaIntro/src/main/java/com/introjava/Exercise8DotOne.java
@@ -0,0 +1,14 @@
+package com.introjava;
+
+
+public class Exercise8DotOne {
+    public static void main(String[] args){
+        double a =2; double b = 4; double c=5; double d=10;
+        double sum = a + b + c + d;
+        System.out.println(sum/4);
+
+
+    }
+
+
+}
